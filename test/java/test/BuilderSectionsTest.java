c64d7a1f5984e9b95c77ae4d1421abc69517340a
Малышева Елена Сергеевна <lena.malicheva@mail.ru>
2024-10-12 22:50:42 +0400

    add new test

---
+++ test/java/test/BuilderSectionsTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+import test.WebDriverParams;
+
+import static io.qameta.allure.Allure.step;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Epic("Тесты конструктора")
+@Feature("Конструктор")
+@Story("Переходы к разделам")
+@Owner("Lenok1998")
+public class BuilderSectionsTest extends WebDriverParams {
+    @Test
+    @Disabled
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Описание теста: Работают переходы к разделам - Булки, Соусы, Начинки")
+    @DisplayName("Раздел 'Конструктор'. Работают переходы к разделам - Булки, Соусы, Начинки")
+    @Tag("automated")
+    public void jumpToIngredientsSection_auto() {
+        open(MainPage.URL);
+        MainPage mainPage = new MainPage();
+        mainPage.clickFillingHeader();
+        assertTrue(mainPage.fillingSectionSelected());
+        mainPage.clickSauceHeader();
+        assertTrue(mainPage.sauceSectionSelected());
+        mainPage.clickBunHeader();
+        assertTrue(mainPage.bunSectionSelected());
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Описание теста: Работают переходы к разделам - Булки, Соусы, Начинки")
+    @DisplayName("Раздел 'Конструктор'. Работают переходы к разделам - Булки, Соусы, Начинки")
+    @Tag("manual")
+    public void jumpToIngredientsSection() {
+        step("Открыть главную страницу" + MainPage.URL);
+        step("Клик на раздел 'Начинки'");
+        step("Проверка что выбран раздел 'Начинки'");
+        step("клик на раздел 'Соусы'");
+        step("проверка что выбран раздел 'Соусы'");
+        step("клик на раздел 'Булки'");
+        step("проверка что выбран раздел 'Булки'");
+    }
+}
---
+++ test/java/test/RegistrationTest.java
@@ -0,0 +1,93 @@
+package test;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.*;
+import service.APIServices;
+import com.github.javafaker.Faker;
+import model.UserAccount;
+import pages.AppHeaderPage;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.ProfilePage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Epic("Тесты профиля")
+@Feature("Профиль")
+@Story("Переходы и взаимодействия в профиле")
+@Owner("Lenok1998")
+@Tag("automated")
+public class RegistrationTest extends WebDriverParams {
+    private final Faker faker = new Faker(new Locale("en"));
+    private final APIServices apiServices = new APIServices();
+    private final List<UserAccount> testData = new ArrayList<>();
+    private UserAccount account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new UserAccount().
+                setEmail(faker.internet().emailAddress()).
+                setPassword(faker.internet().password()).
+                setName(faker.name().firstName());
+        testData.add(account);
+        apiServices.createAccount(account);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        apiServices.deleteAccounts(testData);
+    }
+
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Описание теста: Проверка перехода в личный кабинет по клику на \"Личный кабинет\"")
+    @DisplayName("Проверка перехода в личный кабинет по клику на \"Личный кабинет\"")
+    public void checkEnterProfileFromAppHeader() {
+        open(MainPage.URL);
+        new AppHeaderPage().clickProfileButtonByUnauthorizedUser().login(account.getEmail(), account.getPassword());
+        ProfilePage profilePage = new AppHeaderPage().clickProfileButtonByAuthorizedUser();
+        assertTrue(profilePage.isDisplayed());
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Описание теста: Проверка перехода из личного кабинета в конструктор по клику на \"Конструктор\"")
+    @DisplayName("Переход из личного кабинета в конструктор. Проверка перехода по клику на \"Конструктор\"")
+    public void checkMovingFromProfileToBurgerBuilder() {
+        open(MainPage.URL);
+        new AppHeaderPage().clickProfileButtonByUnauthorizedUser().login(account.getEmail(), account.getPassword());
+        MainPage mainPage = new AppHeaderPage().clickBuilderButton();
+        assertTrue(mainPage.isDisplayed());
+        assertTrue(mainPage.isAuthorisedUser());
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Описание теста: Проверка перехода из личного кабинета в конструктор по клику на логотип \"Stellar Burgers\"")
+    @DisplayName("Переход из личного кабинета в конструктор. Проверка перехода по клику на логотип \"Stellar Burgers\"")
+    public void checkMovingFromProfileToLogo() {
+        open(MainPage.URL);
+        new AppHeaderPage().clickProfileButtonByUnauthorizedUser().login(account.getEmail(), account.getPassword());
+        MainPage mainPage = new AppHeaderPage().clickAppLogo();
+        assertTrue(mainPage.isDisplayed());
+        assertTrue(mainPage.isAuthorisedUser());
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Описание теста: Проверка выхода из аккаунта по кнопке \"Выйти\" в личном кабинете")
+    @DisplayName("Выход из аккаунта. Проверка выхода по кнопке \"Выйти\" в личном кабинете")
+    public void checkExitByExitButtonFromProfile() {
+        open(MainPage.URL);
+        new AppHeaderPage().clickProfileButtonByUnauthorizedUser().login(account.getEmail(), account.getPassword());
+        ProfilePage profilePage = new AppHeaderPage().clickProfileButtonByAuthorizedUser();
+        LoginPage loginPage = profilePage.exitButtonClick();
+        assertTrue(loginPage.isDisplayed());
+    }
+}
